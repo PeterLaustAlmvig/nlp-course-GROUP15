b667cbba231fbd38f8eb5736cf76d226da12d946
jmb-cs <jmba@cs.aau.dk>
2025-10-23 10:15:01 +0200

    Results of the sampling experiments

--- models/part_3/roberta_classifier/finetuning.py
+++ models/part_3/roberta_classifier/finetuning.py
@@ -1,11 +1,7 @@
 import itertools
-import json
 import os
 from sklearn.metrics import confusion_matrix
-import torch
 import argparse
-import logging
-import sys
 import numpy as np
 import pandas as pd
 from transformers import (
